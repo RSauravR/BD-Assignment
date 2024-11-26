b7787c7b96efbf7a26fe90536091c74f13d82dd3
RSauravR <rmnsaurav@gmail.com>
2024-11-26 10:31:35 +0530

    corrected 2

--- index.js
+++ index.js
@@ -93,8 +93,8 @@ app.get('/estimate-delivery', (req, res) => {
 });
 
 app.get('/shipping-cost', (req, res) => {
-  let weight = parseFloat(res.query.weight);
-  let distance = parseFloat(res.query.distance);
+  let weight = parseFloat(req.query.weight);
+  let distance = parseFloat(req.query.distance);
   res.send(calShippingCost(weight, distance).toString());
 });
 
